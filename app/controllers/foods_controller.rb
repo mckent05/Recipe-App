156b16c4391a8f8a95d3a1f5e1fc06802a3b46a9
Wes-Isaac <yisehakmitiku@gmail.com>
2022-03-09 14:11:02 +0800

    Add actions index new create destroy for food_controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,29 @@
 class FoodsController < ApplicationController
-    def index
-        
-    end
+	def index
+		food = Food.all
+	end
+
+	def new
+		@food = Food.new
+	end
+
+	def create
+		food = current_user.food.new(post_params)
+		respond_to do |format|
+			format.html do
+				if food.save
+					flash[:success] = 'Created New Food Succesfully!'
+					redirect_to food_path
+				else
+					render :new, alert: 'Error occured!'
+				end
+			end
+		end
+	end
+
+	def destroy
+		food = Food.find([:id])
+		food.destroy
+		redirect_to food_path
+	end
 end
